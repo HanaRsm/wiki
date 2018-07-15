87979080ceef053f762b8504040908e0f8d64c04
HanaRsm <h.alrassam@live.com>
2018-07-15 19:32:29 +0300

    done with wiki

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Page(models.Model):
+	title = models.CharField(max_length=120)
+	content = models.CharField(max_length=120)
+	last_update = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse('page-detail', kwargs={"page_id":self.id})
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
+def list(request):
+	context = {
+		'page':Page.objects.all()
+	}
+	return render(request, 'list.html', context)
+
+
+def Detail(request, page_id):
+	context = {
+		'page': Page.objects.get(id=page_id)
+	}
+	return render(request, 'detail.html', context)
